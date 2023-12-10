033606dcddc3a3e144137d6d112165233fd49cfb
Vibhu Dawar <9vibhu@gmail.com>
2023-12-11 01:26:52 +0530

    react useState hook

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+
+function App() {
+  
+  // use state hook returns the array and first element is variable and second being the function
+  // useState hook takes the initial value of the variable as the input
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  }
+  const removeValue = () => {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>React Course with Hitesh</h1>
+      <h2>Counter value: {counter} </h2>
+      <button onClick={addValue}>Add Value</button>{" "}
+      <button onClick={removeValue}>Remove Value</button>
+      <footer>footer: {counter} </footer>
+    </>
+  );
+}
+
+export default App;
